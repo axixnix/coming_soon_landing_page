d9e1b1aa237ad94565daacbb3ed16f522d2ccd22
axixnix <henryirons@yahoo.com>
2019-03-18 11:41:11 +0100

    added some javascript for the calculation of the sections in the countdown timer

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 const countdown = document.querySelector('.countdown')
 
-//set launch date (milliseconds)
+// set launch date (milliseconds)
 const launchDate = new Date('Oct 29,2019 13:00:00').getTime()
 
-//update every second
-const intvl = setInterval(()=>{
-    //get today's date and time (milliseconds)
-    const now = new Date().getTime
+// update every second
+const intvl = setInterval(() => {
+  // get today's date and time (milliseconds)
+  const now = new Date().getTime
 
-    //distance from now to the launch date
-    const distance = launchDate - now
+  // distance from now to the launch date
+  const distance = launchDate - now
 
-},1000)
+  // time calculations
+  const days = math.floor(distance / (1000 * 60 * 60 * 24))
+
+  const hours = math.floor((distance % (1000 * 60 * 60 * 24))) / (1000 * 60 * 60)
+
+  const minutes = math.floor(distance % (1000 * 60 * 60)) / (1000 * 60)
+
+  const seconds = math.floor(distance % (1000 * 60)) / (1000)
+}, 1000)
