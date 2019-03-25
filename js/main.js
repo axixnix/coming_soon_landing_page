e74a8f56e8b895016b652564202bc1b027ef4eea
axixnix <henryirons@yahoo.com>
2019-03-25 20:56:03 +0100

    just used liveserver to check my work and realized that the countdown div was not displaying, got it to display, found out the divs within the countdown were not approximating properly, corrected all the mistakes

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
 const countdown = document.querySelector('.countdown')
 
 // set launch date (milliseconds)
-const launchDate = new Date('Oct 29,2019 13:00:00').getTime()
+const launchDate = new Date('Jan 29,2020 13:00:00').getTime()
+
+console.log(launchDate)
 
 // update every second
 const intvl = setInterval(() => {
   // get today's date and time (milliseconds)
-  const now = new Date().getTime
+  const now = new Date().getTime()
 
   // distance from now to the launch date
   const distance = launchDate - now
 
   // time calculations
-  const days = math.floor(distance / (1000 * 60 * 60 * 24))
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24))
 
-  const hours = math.floor((distance % (1000 * 60 * 60 * 24))) / (1000 * 60 * 60)
+  const hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
 
-  const minutes = math.floor(distance % (1000 * 60 * 60)) / (1000 * 60)
+  const minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60))
 
-  const seconds = math.floor(distance % (1000 * 60)) / (1000)
+  const seconds = Math.floor(distance % (1000 * 60) / (1000))
 
   // display result
   countdown.innerHTML = `
